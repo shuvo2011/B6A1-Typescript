34467ea9c4a477fa7a94e6afa77719e2bb543f4e
shuvo2011 <shuvombstu2011@gmail.com>
2025-12-12 12:35:00 +0600

    done problem 1

---
+++ solution.ts
@@ -0,0 +1,11 @@
+function formatValue(value: string | number | boolean): string | number | boolean {
+	if (typeof value === "string") {
+		return value.toUpperCase();
+	}
+
+	if (typeof value === "number") {
+		return value * 10;
+	}
+
+	return !value;
+}
