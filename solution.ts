5f8ef41f7b2d8659937beea375c23e7d29b49034
shuvo2011 <shuvombstu2011@gmail.com>
2025-12-14 12:00:37 +0600

    solved problem 2 and problem 3

--- solution.ts
+++ solution.ts
@@ -9,3 +9,29 @@ function formatValue(value: string | number | boolean): string | number | boolea
 
 	return !value;
 }
+
+function getLength(value: string | unknown[]): number {
+	if (typeof value === "string") {
+		return value.length;
+	}
+
+	if (Array.isArray(value)) {
+		return value.length;
+	}
+
+	return 0;
+}
+
+class Person {
+	name: string;
+	age: number;
+
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+
+	getDetails(): string {
+		return `Name: ${this.name}, Age: ${this.age}`;
+	}
+}
